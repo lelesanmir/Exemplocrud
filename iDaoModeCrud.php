249bfc96a290e0909861c59b96e5fe5bebc2a553
Leonardo dos Santos Miranda <91301972+lelesanmir@users.noreply.github.com>
2023-04-04 12:50:05 -0300

    Add files via upload

---
+++ iDaoModeCrud.php
@@ -0,0 +1,7 @@
+<?php
+interface iDaoModeCrud {
+    public function create( $object );
+    public function read( $param );
+    public function update( $object );
+    public function delete( $param );
+}
